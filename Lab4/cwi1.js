43a0f62ab69166b3e20f0edda03208a38305f381
Xenvar2307 <lukaszkniemczyk@gmail.com>
2024-04-11 11:53:10 +0200

    Lab4 part 1

---
+++ Lab4/cwi1.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName('body')[0].innerHTML += "Mój skrypt"
---
+++ Lab4/cwi2.js
@@ -0,0 +1,2 @@
+const children = document.getElementById("kontener").getElementsByTagName("div");
+document.getElementById("info").textContent = "Liczba kart: " + children.length;
