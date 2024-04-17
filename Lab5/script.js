800e071405966970e4923a430bc1d6109e685f75
Xenvar2307 <lukaszkniemczyk@gmail.com>
2024-04-17 23:11:57 +0200

    Lab5 almost done zad1

---
+++ Lab5/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    let local = 3; // zmienna lokalna
+    global = 5; // zmienna globalna (używa istniejącej lub
+    // tworzy nową)
+    console.log("Debug info");
+});
