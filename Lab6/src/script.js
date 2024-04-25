6690d4c4ad179f6032d4886d20e824a9806497dd
Xenvar2307 <lukaszkniemczyk@gmail.com>
2024-04-25 13:41:10 +0200

    Lab6 Done

--- Lab6/src/script.js
+++ Lab6/src/script.js
@@ -42,7 +42,9 @@ function getBitcoinPrice(e) {
 const todoUrl = 'https://api.restful-api.dev/objects';
 
 let todoDb = {
-    "id": "abcd1234",
+    //won't work, cant set id of an object in POST, it is generated automaticly
+    "id": "abc123",
+    //rest of data
     name: "My TODOs",
     data: {
         nextId: 0,
@@ -89,6 +91,13 @@ function loadTodoList(dbId) {
                 console.log(xhttp.responseText)
                 todoDb = JSON.parse(xhttp.responseText);
                 dbIdElement.value = todoDb.id;
+                //here load divs for loaded database
+                //first add priority tasks
+                todoDb.data.todo.filter(function (task) { return task.priority }).forEach(addTaskToDoDiv)
+                //then add nonpriority tasks
+                todoDb.data.todo.filter(function (task) { return !task.priority }).forEach(addTaskToDoDiv)
+                // and done tasks
+                todoDb.data.done.forEach(addTaskDoneDiv)
             } else {
                 alert("Błąd podczas pobierania bazy: " + xhttp.responseText)
             }
@@ -99,15 +108,117 @@ function loadTodoList(dbId) {
     xhttp.send();
 }
 
+function addTaskToDoDiv(task) {
+    const kontener = document.getElementById("To Do");
+    const kontenerDone = document.getElementById("Did Do");
+    const Task = createDivTask(task.desc, task.id, task.priority);
 
-function addTask() {
-    let desc = document.getElementById("taskName").value;
-    let newId = todoDb.nextId++;
-    todoDb.data.todo.push({ id: newId, desc: desc });
-    saveDb();
+    kontener.appendChild(Task);
 }
 
+function addTaskDoneDiv(task) {
+    const kontener = document.getElementById("To Do");
+    const kontenerDone = document.getElementById("Did Do");
+    const Task = createDivTask(task.desc, task.id, false);
+    Task.classList.add("done");
+
+    kontenerDone.appendChild(Task);
+}
+
+
+function addTask() {
+    // add to db
+    let desc = document.getElementById("taskName").value;
+    let newId = todoDb.data.nextId++;
+    todoDb.data.todo.push({ id: newId, desc: desc, priority: false });
+    saveDb();
+
+    //add div
+    const kontener = document.getElementById("To Do");
+    const kontenerDone = document.getElementById("Did Do");
+    const Task = createDivTask(desc, newId, false);
+    //Task.onclick = selectCard;
+    kontener.appendChild(Task);
+}
+
+function createDivTask(name, id, priority) {
+
+    const Task = document.createElement('div');
+    Task.id = id;
+    Task.textContent = name;
+    Task.className = "task";
+    if (priority) {
+        Task.classList.add("priority")
+    }
+
+    Task.addEventListener("dblclick", function (event) {
+        const kontener = document.getElementById("To Do");
+        const kontenerDone = document.getElementById("Did Do")
+        if (kontener.contains(Task)) {
+            // here add/remove priority to db too
+            if (Task.classList.contains("priority")) {
+                Task.classList.remove("priority");
+                todoDb.data.todo.find(function (task) { return (task.id == Task.id); }).priority = false;
+                saveDb();
+                kontener.appendChild(Task)
+            } else {
+                Task.classList.add("priority")
+                todoDb.data.todo.find(function (task) { return (task.id == Task.id); }).priority = true;
+                saveDb();
+                kontener.insertBefore(Task, kontener.firstElementChild)
+            }
+        }
+    })
+
+    const btn2 = document.createElement("button");
+    btn2.textContent = "Done";
+    btn2.onclick = () => {
+        //here move to another list in db too
+        const kontener = document.getElementById("To Do");
+        const kontenerDone = document.getElementById("Did Do")
+        if (!kontenerDone.contains(Task)) {
+            Task.classList.add("done");
+            Task.classList.remove("priority")
+            kontenerDone.appendChild(Task);
+
+            moved_task = (todoDb.data.todo.find(function (task) { return (task.id == Task.id); }))
+            moved_task.priority = false;
+            todoDb.data.done.push(moved_task)
+            todoDb.data.todo.splice(todoDb.data.todo.indexOf(moved_task), 1)
+            saveDb();
+        }
+    }
+    Task.appendChild(btn2);
+
+    const btn1 = document.createElement("button");
+    btn1.textContent = "Delete";
+    btn1.onclick = () => {
+        //remove from db too
+        const kontener = document.getElementById("To Do");
+        const kontenerDone = document.getElementById("Did Do")
+        if (kontener.contains(Task)) {
+            removed_task = (todoDb.data.todo.find(function (task) { return (task.id == Task.id); }))
+            todoDb.data.todo.splice(todoDb.data.done.indexOf(removed_task), 1)
+            saveDb();
+            kontener.removeChild(Task);
+        }
+        else {
+            removed_task = (todoDb.data.done.find(function (task) { return (task.id == Task.id); }))
+            todoDb.data.done.splice(todoDb.data.done.indexOf(removed_task), 1)
+            saveDb();
+            kontenerDone.removeChild(Task);
+        }
+    }
+    Task.appendChild(btn1);
+
+    return Task;
+}
+
+//POTENTIAL ERROR, when actions happen fast, sometimes previous save is done after future save,
+// resulting in overriding current data with previous state of data from past save
+
 function saveDb() {
+    // check for interuption?
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
